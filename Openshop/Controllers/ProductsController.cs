cf47c35449bf4bc3c1e411eeb3391972e8878b4f
unknown <subaraj1961@gmail.com>
2022-05-26 23:03:52 +0530

    first commit

---
+++ Openshop/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Openshop.Controllers
+{
+    [Route("api/abc")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public string GetProducts(int id)
+        {
+            return "List of Products";
+        }
+    }
+}
